cbfcd0d25c7d91ee1151007fbc063f9c5af28dcf
shumpei-kayano <shumpei.kayano@mail.o-hara.ac.jp>
2023-06-04 08:17:50 +0900

    Memoモデルの作成と管理サイトへの登録

--- django_memo/app/models.py
+++ django_memo/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Memo(models.Model):
+    title = models.CharField(max_length=150)
+    text = models.TextField(blank=True)
+    # モデルインスタンスが初めて作成されたときにフィールドが現在の日時で自動的に設定
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    # モデルインスタンスが保存されるたびにフィールドが現在の日時で自動的に更新
+    updated_datetime = models.DateTimeField(auto_now=True)
