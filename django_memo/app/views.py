e1385d4c1b8cbab5e776ebe4f60690c3624ad086
shumpei-kayano <shumpei.kayano@mail.o-hara.ac.jp>
2023-06-04 10:20:51 +0900

    新規登録処理の作成

--- django_memo/app/views.py
+++ django_memo/app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Memo
+from .forms import MemoForm
 from django.shortcuts import get_object_or_404
+
 
 # トップページの表示
 def index(request):
@@ -14,4 +16,22 @@ def detail(request, memo_id):
 
 # メモ新規ページ
 def new_memo(request):
-    return render(request, 'app/new_memo.html')
+    # POST送信でnew_memoにアクセスした時
+    if request.method == "POST":
+        # ユーザー入力情報を元にMemoFormインスタンスの生成
+        form = MemoForm(request.POST)
+        '''
+        if form.is_valid():では、生成されたMemoインスタンスが正しい値を持っているかを検証している。
+        Memoモデルは、タイトルが150文字より多かったりタイトルがblankではいけないなどの条件を指定しているが
+        インスタンスがこの条件を満たしているかを判定している
+        '''
+        if form.is_valid():
+            form.save()
+            return redirect('app:index')
+    else:
+        '''
+        MemoFormをインポートして、変数formに代入
+        これで、new_memo.htmlでは、{{ form }}という記述でフォームが表示されるようになる。
+        '''
+        form = MemoForm
+    return render(request, 'app/new_memo.html', {'form': form })
